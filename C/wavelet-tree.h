733882103a663836cd1d262c9b1167ffafbd1560
dsisejkovic <dominik.sisejkovic@gmail.com>
2016-01-09 23:13:19 +0100

    Added alphabet extraction

---
+++ C/wavelet-tree.h
@@ -0,0 +1,16 @@
+//
+// Created by dsisejkovic on 09.01.16..
+//
+
+#ifndef C_WAVELET_TREE_H
+#define C_WAVELET_TREE_H
+
+
+struct WaveletNode {
+    struct WaveletNode *left_child;
+    struct WaveletNode *right_child;
+
+};
+
+
+#endif //C_WAVELET_TREE_H
