4debbd0063a53c5fdf7debb48c6c43da5c12caf7
dsisejkovic <dominik.sisejkovic@gmail.com>
2016-01-10 21:57:12 +0100

    Added main function and args

--- C/main.c
+++ C/main.c
@@ -1,25 +1,79 @@
 //
 // Created by dsisejkovic on 09.01.16..
 //
+#include <stdio.h>
 #include "Basic.h"
 #include "WaveletTree.h"
-#include "Utils.h"
 #include "Tester.h"
 
 
-int main() {
-//    const char *file_name = "input.txt";
-//    char *input;
-//    char *used_alphabet;
-//    getAlphabetFromFile(file_name, &used_alphabet, &input);
-//    printf("%s\n", input);
-//    printf("%s\n", used_alphabet);
+/**
+ * Arguments: <input-file-path> <query-type> <args>
+ * - <input-file-path> access <index>
+ * - <input-file-path> rank <character> <index>
+ * - <input-file-path> select <character> <index>
+ * - test
+ */
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        printf("|c-wavelet tree instructions\n");
+        printf("|---------------------------\n");
+        printf(" - <input-file-path> access <index>\n");
+        printf(" - <input-file-path> rank <index> <character>\n");
+        printf(" - <input-file-path> select <index> <character>\n");
+        printf(" - test\n");
+        return 0;
+    }
 
-    testAll();
-//    struct WaveletTree *tree = buildTree(input, used_alphabet);
-//
-//
-//    free((void *) input);
-//    free((void *) tree);
+    if (strcmp(argv[1], "test") == 0) {
+        testAll();
+    } else {
+        char *file_name = argv[1];
+        char *input;
+        char *used_alphabet;
+        getAlphabetFromFile(file_name, &used_alphabet, &input);
+        struct WaveletTree *tree = buildTree(input, used_alphabet);
+
+        char *query = argv[2];
+        char *result = (char *) malloc(25 * sizeof(char));
+        if (strcmp(query, "access") == 0) {
+            int index = atoi(argv[3]);
+            char access = accessOp(tree, used_alphabet, index);
+
+            snprintf(result, 25, "access(%d) = %c", index, access);
+            printf("|access(%d) = %c\n", index, access);
+        } else if (strcmp(query, "rank") == 0) {
+            int index = atoi(argv[3]);
+            char *character = argv[4];
+            int rank = rankOp(tree, used_alphabet, index, *character);
+
+            snprintf(result, 25, "rank(%d, %c) = %d", index, *character, rank);
+            printf("|rank(%d, %c) = %d\n", index, *character, rank);
+        } else if (strcmp(query, "select") == 0) {
+            int index = atoi(argv[3]);
+            char *character = argv[4];
+            int select = selectOp(tree, used_alphabet, *character, index);
+
+            snprintf(result, 25, "select(%d, %c) = %d", index, *character, select);
+            printf("|select(%d, %c) = %d\n", index, *character, select);
+        } else {
+            error("Unknown query given.");
+        }
+
+        char *result_file_name = query;
+        strcat(result_file_name, "-res.txt");
+
+        FILE *file = fopen(result_file_name, "w");
+        fputs(result, file);
+
+        printf("|Result written to file: %s\n", result_file_name);
+
+        free((void *) used_alphabet);
+        free((void *) result);
+        free((void *) input);
+        free((void *) tree);
+        fclose(file);
+    }
+
     return 0;
 }
