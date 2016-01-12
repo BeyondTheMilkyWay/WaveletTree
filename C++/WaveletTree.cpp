015f3d8f3fcd851e41a230b75cef27d74804c734
Domagoj Stankovic <dstankovic@Domagojs-MBP.fritz.box>
2016-01-12 21:12:44 +0100

    [C++] tree build fix

--- C++/WaveletTree.cpp
+++ C++/WaveletTree.cpp
@@ -16,21 +16,31 @@ bool code(std::map<char, bool> &map, std::vector<char> &alphabet, char q) {
   return map[q];
 }
 
-WaveletNodeP build2(std::string &str, int l, int r, std::vector<char> &alphabet) {
+WaveletNodeP build2(std::string &str, std::vector<char> &alphabet) {
   if (alphabet.size() <= 1) {
     return std::make_shared<WaveletNodeNull>(alphabet);
   }
-  BitArray bitArray(r - l + 1);
+  int size = str.size();
+
+  // build bitstring, 0-string and 1-string
+  std::string str0;
+  std::string str1;
+  BitArray bitArray(size);
   std::map<char, bool> map;
-  for (int i = l; i <= r; i++) {
-    bitArray.set(i - l, code(map, alphabet, str[i]));
+  for (int i = 0; i < size; i++) {
+    bool cd = code(map, alphabet, str[i]);
+    bitArray.set(i, cd);
+    if (cd) {
+      str1.push_back(str[i]);
+    } else {
+      str0.push_back(str[i]);
+    }
   }
+
   std::vector<char> lAlphabet(alphabet.begin(), alphabet.begin() + alphabet.size() / 2);
-  int mid = (l + r - 1)/2;
-  std::cout << "l: " << l << " r: " << r << " mid= " << mid << std::endl;
-  WaveletNodeP left = build2(str, l, mid, lAlphabet);
+  WaveletNodeP left = build2(str0, lAlphabet);
   std::vector<char> rAlphabet(alphabet.begin() + alphabet.size() / 2, alphabet.end());
-  WaveletNodeP right = build2(str, mid + 1, r, rAlphabet);
+  WaveletNodeP right = build2(str1, rAlphabet);
   std::shared_ptr<WaveletNode> node = std::make_shared<WaveletNode>(bitArray, alphabet, left, right);
   left->parent = node;
   right->parent = node;
@@ -55,7 +65,7 @@ void WaveletTree::build(std::string &str) {
   std::sort(alphabet.begin(), alphabet.end());
 
   // build tree
-  this->root = build2(str, 0, str.size()-1, alphabet);
+  this->root = build2(str, alphabet);
 }
 
 int WaveletTree::rank(char q, int x) {
